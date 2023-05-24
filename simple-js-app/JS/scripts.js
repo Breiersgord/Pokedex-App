990d4b1e054bd4434ffaa62f1c97f4fa4a8767fe
Breiersgord <128760416+Breiersgord@users.noreply.github.com>
2023-05-23 20:53:56 -0500

    addition of 'ivysaur' & 'venusaur' to variable pokemonlist
    if applied, this commit will add objects 'ivysaur' and 'venusaur' to the pokemonlist variable.

--- simple-js-app/JS/scripts.js
+++ simple-js-app/JS/scripts.js
@@ -1,5 +1,22 @@
 /*JS for Intro to JavaScript*/
 
-let pokemonlist = [
-    {name: 'bulbasaur', height: '71.1cm',  types: ['grass', 'poison'], ndex: '0001'}
+let pokemonlist = [ //pokemon names & stats for pokedex
+    {
+        name: 'bulbasaur', 
+        height: '.711m',  
+        types: ['grass', 'poison'], 
+        ndex: '0001'
+    },
+    {
+        name: 'ivysaur',
+        height: '1m',
+        types: ['grass', 'poison'],
+        ndex: '0002'
+    },
+    {
+        name: 'venusaur',
+        height: '2m',
+        types: ['grass', 'poison'],
+        ndex: '0003'
+    }
 ]
