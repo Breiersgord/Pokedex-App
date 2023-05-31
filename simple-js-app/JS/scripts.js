4680ab16406c08767ef0b7bb465f4ad3852b1728
Breiersgord <128760416+Breiersgord@users.noreply.github.com>
2023-05-30 21:54:14 -0500

    loop creation for pokemon height
    if applied, this commit will create a for/else loop addressing a pokemon's height with a corresponding remark to their height

--- simple-js-app/JS/scripts.js
+++ simple-js-app/JS/scripts.js
@@ -19,4 +19,14 @@ let pokemonlist = [ //pokemon names & stats for pokedex
         types: ['grass', 'poison'],
         ndex: 0003
     }
-]
+];
+
+for (let i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].height >=2){
+        document.write(pokemonList[i].name + pokemonList[i].height + "- Wow, this is big!");
+    }else if (pokemonList[i].height <2 && pokemonList[i].height >=0){
+        document.write(pokemonList[i].name + pokemonList[i].height + "- This is an average size.");
+    } else {
+        document.write(pokemonList[i].name + pokemonList[i].height + "- Oh! This is very small.");
+    }
+    }
