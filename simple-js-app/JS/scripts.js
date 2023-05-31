3d7f85c208a1a7c065df377f59ca591db39d0358
Breiersgord <128760416+Breiersgord@users.noreply.github.com>
2023-05-30 21:45:36 -0500

    Corrected NDEX to reflect a 'number'
    in applied, this commit will update the section 'ndex' to reflect as a number

--- simple-js-app/JS/scripts.js
+++ simple-js-app/JS/scripts.js
@@ -5,18 +5,18 @@ let pokemonlist = [ //pokemon names & stats for pokedex
         name: 'bulbasaur', 
         height: '.711m',  
         types: ['grass', 'poison'], 
-        ndex: '0001'
+        ndex: 0001
     },
     {
         name: 'ivysaur',
         height: '1m',
         types: ['grass', 'poison'],
-        ndex: '0002'
+        ndex: 0002
     },
     {
         name: 'venusaur',
         height: '2m',
         types: ['grass', 'poison'],
-        ndex: '0003'
+        ndex: 0003
     }
 ]
