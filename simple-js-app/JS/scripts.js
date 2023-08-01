6c8c3f352c7c3e8083bfced710abacd79a2687fe
Bowie <128760416+Breiersgord@users.noreply.github.com>
2023-08-01 13:39:49 -0500

    update pokemonList 'for loop'
    if applied, this commit will update the 'for loop' to a 'forEach Loop'

--- simple-js-app/JS/scripts.js
+++ simple-js-app/JS/scripts.js
@@ -45,20 +45,20 @@ let pokemonRepository = (function () {
 
 // printArrayDetails function declaration
 function printArrayDetails(list){
-    for (let i = 0; i < pokemonList.length; i++) {
-        if (pokemonList[i].height >=2){
+    pokemonList.forEach(function(pokemon) {
+        if (pokemon.height >=2){
             //document.write(pokemonList[i].ndex + pokemonList[i].name + pokemonList[i].height + pokemonList[i].types + "- Wow, this is big!");
-            document.write('<p> ' + pokemonList[i].ndex + pokemonList[i].name + ": " + pokemonList[i].height + "m," + pokemonList[i].types + "- Wow, this is big!" + ' </p>')
+            document.write('<p> ' + pokemon.ndex + pokemon.name + ": " + pokemon.height + "m," + pokemon.types + "- Wow, this is big!" + ' </p>')
 
-        } else if (pokemonList[i].height <2 && pokemonList[i].height >=1){
+        } else if (pokemon.height <2 && pokemon.height >=1){
             //document.write(pokemonList[i].ndex + pokemonList[i].name + pokemonList[i].height + pokemonList[i].types + "- This is an average size.");
-            document.write('<p> ' + pokemonList[i].ndex + pokemonList[i].name + ": " + pokemonList[i].height + "m," + pokemonList[i].types + "- This is an average size." + ' </p>')
+            document.write('<p> ' + pokemon.ndex + pokemon.name + ": " + pokemon.height + "m," + pokemon.types + "- This is an average size." + ' </p>')
 
         } else {
             //document.write(pokemonList[i].ndex + pokemonList[i].name + pokemonList[i].height + pokemonList[i].types + "- Oh! This is very small.");
-            document.write('<p> ' + pokemonList[i].ndex + pokemonList[i].name + ": " + pokemonList[i].height + "m," + pokemonList[i].types + "- Oh! This is very small." + ' </p>')
+            document.write('<p> ' + pokemon.ndex + pokemon.name + ": " + pokemon.height + "m," + pokemon.types + "- Oh! This is very small." + ' </p>')
         }
-    }
+    });
 }
 
 printArrayDetails(pokemonList); // executes the function using ‘pokemonList‘ as its input
